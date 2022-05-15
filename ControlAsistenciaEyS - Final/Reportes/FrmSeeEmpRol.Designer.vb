7c205df5290afcc76c65b3a7e3d979b693726d11
ErnestoRoman <95254370+ErnestoRoman@users.noreply.github.com>
2022-05-15 17:49:28 -0600

    Reporte Empleado Rol Creado

--- ControlAsistenciaEyS - Final/Reportes/FrmSeeEmpRol.Designer.vb
+++ ControlAsistenciaEyS - Final/Reportes/FrmSeeEmpRol.Designer.vb
@@ -22,9 +22,106 @@ Partial Class FrmSeeEmpRol
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtEmpSelct = New System.Windows.Forms.TextBox()
+        Me.BtnAsigRol = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtBuscar = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(454, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Reporte y Manejo de Roles de Empleados"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(223, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Empleado Seleccionado:"
+        '
+        'TxtEmpSelct
+        '
+        Me.TxtEmpSelct.BackColor = System.Drawing.SystemColors.Control
+        Me.TxtEmpSelct.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtEmpSelct.Location = New System.Drawing.Point(22, 108)
+        Me.TxtEmpSelct.Name = "TxtEmpSelct"
+        Me.TxtEmpSelct.Size = New System.Drawing.Size(207, 29)
+        Me.TxtEmpSelct.TabIndex = 2
+        '
+        'BtnAsigRol
+        '
+        Me.BtnAsigRol.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAsigRol.Location = New System.Drawing.Point(247, 106)
+        Me.BtnAsigRol.Name = "BtnAsigRol"
+        Me.BtnAsigRol.Size = New System.Drawing.Size(189, 31)
+        Me.BtnAsigRol.TabIndex = 3
+        Me.BtnAsigRol.Text = "Asignar Rol"
+        Me.BtnAsigRol.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 24)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Buscar:"
+        '
+        'TxtBuscar
+        '
+        Me.TxtBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtBuscar.Location = New System.Drawing.Point(84, 168)
+        Me.TxtBuscar.Name = "TxtBuscar"
+        Me.TxtBuscar.Size = New System.Drawing.Size(686, 29)
+        Me.TxtBuscar.TabIndex = 5
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 218)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(775, 278)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'FrmSeeEmpRol
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 508)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TxtBuscar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.BtnAsigRol)
+        Me.Controls.Add(Me.TxtEmpSelct)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FrmSeeEmpRol"
         Me.Text = "FrmSeeEmpRol"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtEmpSelct As TextBox
+    Friend WithEvents BtnAsigRol As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtBuscar As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
